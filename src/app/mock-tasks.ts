38c3b16a0498574ee3daba2aa1f179d89354dc10
adco23 <adco2393@gmail.com>
2022-03-14 00:25:02 -0300

    Initial commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,27 @@
+import { Task } from "./Task"
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: "Terminar el primer modulo de Angular",
+    day: "Agosto 5 a las 12:00 pm",
+    reminder: true
+  },
+  {
+    id: 2,
+    text: "Merendar",
+    day: "Agosto 6 a las 12:00 pm",
+    reminder: true
+  },
+  {
+    id: 3,
+    text: "Investigar",
+    day: "Agosto 5 a las 17:00 pm",
+    reminder: false
+  },
+  {
+    id: 4,
+    text: "Dibujar",
+    day: "Agosto 10 a las 12:00 pm",
+    reminder: false
+  },
+]
